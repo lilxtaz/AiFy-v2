36b430ba9a42032d53d9c55cdb3503e1df3a6cff
lilxtaz <122042405+lilxtaz@users.noreply.github.com>
2023-03-05 21:03:47 +0100

    fixes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className={`px-64 py-16 relative`}>
 
-        <div className="absolute w-[50%] inset-0 gradient-01" />
+        <div className=" w-[50%] inset-0 gradient-01" />
             <div className={`${styles.innerWidth} m-auto flex justify-between items-center gap-8`}>
             <img
                 src="/logo.svg"
@@ -18,12 +18,14 @@ const Navbar = () => {
                 className={`${styles.iconsStyles} pb-3`} 
                 width={96}
             />
-            <div className="font-thin text-[24px] text-black flex items-center justify-center leading-7 mx-auto">
-                <Link href='#' className='px-6 font-medium cursor-pointer'>Home</Link>
-                <Link href='#' className='px-6 font-medium cursor-pointer'>About Us</Link>
-                <Link href='#' className='px-6 font-medium cursor-pointer'>Contact</Link>
+            <div className="font-thin text-[24px] text-black flex items-center justify-center leading-7  ">
+
+                <Link href='/black' className='mx-6 font-medium cursor-pointer'>Home</Link>
+                <Link href='/white' className='mx-6 font-medium cursor-pointer'>About Us</Link>
+                <Link href='/grey' className='mx-6 font-medium cursor-pointer'>Contact</Link>
             </div>
             <Buttons text='Get App'/>
+            
             </div>
 
         
