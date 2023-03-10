9fb482092439be162d91bb62d9d291643d78b838
lilxtaz <122042405+lilxtaz@users.noreply.github.com>
2023-03-10 15:13:15 +0100

    img changes

--- sections/Features.jsx
+++ sections/Features.jsx
@@ -39,7 +39,7 @@ const Features = () => {
             
 
              priority={true}
-             src="/iphone.png" style={{transform:'scale(1.3)'}} 
+             src="/iphone.png" style={{transform:'scale(1.7)'}} 
              className='pl-4'
              width={400}
              height={400}
