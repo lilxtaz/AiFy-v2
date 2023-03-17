a9bc717ac9b4399b5b44786d6077dde64bdf4015
lilxtaz <122042405+lilxtaz@users.noreply.github.com>
2023-03-17 23:07:02 +0100

    working on blog

--- components/Buttons.jsx
+++ components/Buttons.jsx
@@ -6,8 +6,8 @@ const Buttons = (props) => {
   return (
     <div class='flex items-center justify-center font-extrabold'>
     <div class="flex items-center justify-center bg-transparent rounded">
-        <button class='relative inline-flex text-lg sm:text-[1.25rem] rounded-full font-medium tracking-wider border-2 border-transparent transition-colors outline-transparent focus:outline-transparent  disabled:pointer-events-none disabled:opacity-40 disabled:hover:opacity-40 disabled:cursor-not-allowed disabled:shadow-none
-        text-text-color bg-white hover:bg-black hover:text-white focus:border-[#B3B3FD] px-4 py-2 sm:py-3 sm:px-8'>
+        <button class={`relative inline-flex text-lg sm:text-[1.25rem] rounded-full font-medium tracking-wider border-2 border-transparent transition-colors outline-transparent focus:outline-transparent  disabled:pointer-events-none disabled:opacity-40 disabled:hover:opacity-40 disabled:cursor-not-allowed disabled:shadow-none
+        text-${props.textMain} bg-${props.main} hover:bg-${props.hover} hover:text-${props.textHover} focus:border-[#B3B3FD] px-4 py-2 sm:py-3 sm:px-8`}>
             {props.text}
         </button>
     </div>
@@ -16,4 +16,5 @@ const Buttons = (props) => {
 }
 
 export default Buttons
+
 
