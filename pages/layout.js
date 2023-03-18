47c85a5c242d7848f4f9dda4da2d73e02e719093
lilxtaz <122042405+lilxtaz@users.noreply.github.com>
2023-03-18 21:54:23 +0100

    j

--- pages/layout.js
+++ pages/layout.js
@@ -1,14 +1,14 @@
-import '../../styles/globals.css'
+// import '../../styles/globals.css'
 
 export const metadata = {
-  title: 'AiFy',
-  description: 'The next step into the future',
-}
+  title: "AiFy",
+  description: "The next step into the future",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
